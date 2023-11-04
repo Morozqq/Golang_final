130d8b87c121bf83b43d2376c023486d14ab220d
Morozqq <aslan.dzheleubaj.04@gmail.com>
2023-11-04 16:56:26 +0600

    Added 2 option console. Added list of flowers

--- main.go
+++ main.go
@@ -1,12 +1,58 @@
 package main
 
+import "fmt"
+
 func main() {
-	//test of command pattern
-	rose := &Flower{Name: "Роза", IsDry: true}
-	waterCommand := &WaterFlowerCommand{Flower: rose}
+	flowers := []Flower{
+		{Name: "Роза", IsDry: true},
+		{Name: "Тюльпан", IsDry: false},
+		{Name: "Лилия", IsDry: true},
+		{Name: "Георгина", IsDry: true},
+		{Name: "Подсолнух", IsDry: false},
+	}
 
-	gardener := &GardenerInvoker{}
-	gardener.AddCommand(waterCommand)
+	waterCommands := make([]Command, 0)
+	for _, flower := range flowers {
+		waterCommands = append(waterCommands, &WaterFlowerCommand{Flower: &flower})
+	}
 
-	gardener.ExecuteCommands()
+	fmt.Println("Welcome to the Flower Care Console:")
+	for {
+		fmt.Println("Options:")
+		fmt.Println("1 - Water a flower")
+		fmt.Println("0 - Exit")
+
+		var choice int
+		fmt.Print("Enter your choice: ")
+		_, err := fmt.Scan(&choice)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return
+		}
+
+		switch choice {
+		case 1:
+			fmt.Println("Choose a flower to water:")
+			for i, flower := range flowers {
+				fmt.Printf("%d - %s\n", i+1, flower.Name)
+			}
+			var flowerChoice int
+			fmt.Print("Enter the number of the flower to water: ")
+			_, err := fmt.Scan(&flowerChoice)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				return
+			}
+			if flowerChoice > 0 && flowerChoice <= len(flowers) {
+				waterCommands[flowerChoice-1].Execute()
+			} else {
+				fmt.Println("Invalid flower choice.")
+			}
+		case 0:
+			fmt.Println("Exiting Flower Care Console.")
+			return
+		default:
+			fmt.Println("Invalid choice. Please choose a valid option.")
+		}
+	}
 }
