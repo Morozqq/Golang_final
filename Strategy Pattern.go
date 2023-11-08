d69cbab3cea53a9d1e9f47b9eef7968fc5e792ba
AidanaIbrayeva <125108139+AidanaIbrayeva@users.noreply.github.com>
2023-11-08 21:04:17 +0600

    Update Strategy Pattern.go

--- Strategy Pattern.go
+++ Strategy Pattern.go
@@ -2,14 +2,21 @@ type State interface {
     Water(flower *Flower)
 }
 
-type DryState struct{}
+type DryState struct{
+	Name  string
+	IsDry bool
+	State FlowerState
+}
 
 func (s *DryState) Water(flower *Flower) {
     fmt.Println("Watering the flower...")
     flower.IsDry = false
 }
 
-type HealthyState struct{}
+type HealthyState struct{
+	Name  string
+	State FlowerState
+}
 
 func (s *HealthyState) Water(flower *Flower) {
     fmt.Println("The flower is healthy and doesn't need water.")
